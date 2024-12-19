26f15b6246439aa0aa3678ab614296c1ea3404c7
MarkSuttirak <suttirak.ch@zaviago.com>
2024-12-19 16:35:19 +0700

    create: coming-soon page

---
+++ app/coming-soon/page.tsx
@@ -0,0 +1,10 @@
+const ComingSoon = () => {
+  return (
+    <section>
+      <h1 className="font-recoleta">Zaviago 2.0 is arriving soon</h1>
+      <p>Discover an array of incredible business platforms and be prepared for an exciting wave of new business tools at the horizon. Sign up to our waitlist to be notified when our services are launched!</p>
+    </section>
+  )
+}
+
+export default ComingSoon
